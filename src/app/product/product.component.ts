f79a87862019beb4256d23731db8ab7d825952fe
jessie <jessie@mycompany.com>
2024-08-04 19:46:21 +0800

    [1]

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -14,20 +14,83 @@ export class ProductComponent implements OnInit {
   constructor (private http: HttpClient) {
   }
 
+  @ViewChild("modalAddEdit") modalAddEdit : ElementRef | undefined;
+
   products: Product[] = [];
+  product: Product = new Product();
+  headers = new HttpHeaders({
+    contentType: "application/json"
+  });
 
   ngOnInit(): void {
     this.LoadProduct();
   }
 
   async LoadProduct() {
-    this.http.get<Product[]>("https://api.restful-api.dev/objects").subscribe((res) => {
+    this.http.get<Product[]>("https://api.restful-api.dev/objects").subscribe(res => {
       this.products = res;
     });
   }
 
-  SaveProduct(){
-    alert("saved!");
+  AddEditProduct(product?: Product){
+    if (this.modalAddEdit != null) {
+      if (product == null) {
+        this.product = new Product();
+      }
+      else
+      {
+        this.product = product;
+      }
+     
+      this.modalAddEdit.nativeElement.style.display = "block";
+    }
+  }
+
+  SaveProduct(){ 
+    if (this.modalAddEdit != null) {
+      if (this.product.id == "")
+      {
+        this.http.post("https://api.restful-api.dev/objects", { name: this.product.name}, { headers: this.headers }).subscribe(res => {
+          if (res != null)
+          {
+            console.log(res);
+            alert("Successfully added");
+          }       
+        });
+      }
+      else
+      {
+        this.http.put("https://api.restful-api.dev/objects/" + this.product.id, { name: this.product.name}, { headers: this.headers }).subscribe(res => {
+          if (res != null)
+          {
+            console.log(res);
+            alert("Successfully updated");
+          }       
+        });
+      }
+
+      this.modalAddEdit.nativeElement.style.display = "none";
+    }
+  }
+
+  DeleteProduct(product: Product){
+    if (this.modalAddEdit != null) {
+      this.http.delete("https://api.restful-api.dev/objects/" + "ff808181911c9fd801911d25b58a0073").subscribe(res => {
+        if (res != null)
+        {
+          console.log(res);
+          alert("Successfully deleted");
+        } 
+      });
+
+      this.modalAddEdit.nativeElement.style.display = "none";
+    }
+  }
+
+  CloseAddEdit(){
+    if (this.modalAddEdit != null) {
+      this.modalAddEdit.nativeElement.style.display = "none";
+    }
   }
 }
 
